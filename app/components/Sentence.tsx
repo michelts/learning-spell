1f66459818d9a9b658b863e3c46d44a0e5b24b47
Michel Sabchuk <michel@turbosys.com.br>
2024-04-11 11:22:45 -0300

    Adjust sentence bg color

--- app/components/Sentence.tsx
+++ app/components/Sentence.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from "react";
 export function Sentence({ children }: { children: ReactNode }): JSX.Element {
   return (
     <div className="flex">
-      <div className="relative z-1 rounded-lg border border-white/30 bg-white/10 p-3 font-bold text-rose-100 leading-relaxed">
+      <div className="relative z-1 rounded-lg border border-purple-200/30 bg-purple-200/10 p-3 font-bold text-rose-100 leading-relaxed">
         {children}
       </div>
     </div>
