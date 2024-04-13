73ddc0e46eb0cad9ae90919d2a79d9c39ec7c921
Michel Sabchuk <michel@turbosys.com.br>
2024-04-13 06:57:27 -0300

    Use pink instead of rose
    It looks like red and could be seen as an error.

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -12,14 +12,14 @@ export function Button({
 }: Props): JSX.Element {
   return (
     <button
-      className="group -z-1 relative rounded-xl bg-rose-600 px-4 py-2 font-bold font-mono text-rose-100 text-xl shadow-glow shadow-rose-600/40 disabled:cursor-not-allowed disabled:bg-stone-400 hover:bg-rose-700 disabled:text-stone-600 hover:underline hover:decoration-rose-200/40 hover:decoration-dotted hover:underline-offset-4 disabled:shadow-stone-500/40 hover:shadow-rose-700/50"
+      className="group -z-1 relative rounded-xl bg-pink-600 px-4 py-2 font-bold font-mono text-pink-100 text-xl shadow-glow shadow-pink-600/40 disabled:cursor-not-allowed disabled:bg-stone-400 hover:bg-pink-700 disabled:text-stone-600 hover:underline hover:decoration-pink-200/40 hover:decoration-dotted hover:underline-offset-4 disabled:shadow-stone-500/40 hover:shadow-pink-700/50"
       {...rest}
     >
       {!isLoading ? (
         children
       ) : (
         <div className="flex items-center gap-2">
-          <span className="h-2 w-2 animate-ping rounded-full bg-rose-100 opacity-75 group-disabled:bg-stone-700" />
+          <span className="h-2 w-2 animate-ping rounded-full bg-pink-100 opacity-75 group-disabled:bg-stone-700" />
           {children}
         </div>
       )}
