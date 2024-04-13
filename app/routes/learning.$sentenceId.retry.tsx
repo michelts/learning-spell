fd50fb729bd885ed6a9884c11b389f6a95d4287a
Michel Sabchuk <michel@turbosys.com.br>
2024-04-13 06:06:37 -0300

    Adjust retry mechanism

--- app/routes/learning.$sentenceId.retry.tsx
+++ app/routes/learning.$sentenceId.retry.tsx
@@ -1,15 +1,19 @@
 import { redirect } from "@remix-run/cloudflare";
 import type { ActionFunctionArgs } from "@remix-run/cloudflare";
 import { getSentenceById } from "~/services/sentences";
+import type { Env } from "~/types";
 
-export async function action({ params }: ActionFunctionArgs) {
-  const sentenceId = Number.parseInt(params.sentenceId ?? "");
-  if (Number.isNaN(sentenceId)) {
+export async function action({ params, context }: ActionFunctionArgs) {
+  const sentenceId = params.sentenceId ?? "";
+  if (!sentenceId) {
     throw new Response("", { status: 404 });
   }
-  const sentence = await getSentenceById(sentenceId);
+  const sentence = await getSentenceById({
+    env: context.env as Env,
+    id: sentenceId,
+  });
   if (!sentence) {
     throw new Response("", { status: 404 });
   }
-  return redirect(`/learning/${sentence.sentenceId}`);
+  return redirect(`/learning/${sentence.id}`);
 }
