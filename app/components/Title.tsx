2b880d2ee46d449ac916747ccd88bbd496b30337
Michel Sabchuk <michel@turbosys.com.br>
2024-04-14 14:21:59 -0300

    Denote title is a link

--- app/components/Title.tsx
+++ app/components/Title.tsx
@@ -3,7 +3,7 @@ import type { ReactNode } from "react";
 
 export function Title({ children }: { children: ReactNode }): JSX.Element {
   return (
-    <h1 className="mb-5 font-bold font-mono text-4xl text-rose-100 md:mb-8">
+    <h1 className="mb-5 font-bold font-mono text-4xl text-rose-100 md:mb-8 hover:underline">
       <Link to="/">{children}</Link>
     </h1>
   );
